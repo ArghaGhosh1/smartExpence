9a8caa3654e0aeb3cebdb047ce0c38832fc3d222
ArghaGhosh1 <arghaghosh276@hotmail.com>
2026-05-03 23:41:02 +0530

    hi

--- src/main/java/com/example/SmartExpense/Config/SecurityConfig.java
+++ src/main/java/com/example/SmartExpense/Config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
